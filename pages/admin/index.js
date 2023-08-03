1a9c6f38075e201ac888ef0c43491588ec90eb02
Thuong Nguyen <thaonhucute2001@gmail.com>
2023-08-03 11:29:14 +0700

    update profile

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -6,32 +6,35 @@ import client from "../../configs/axios.config";
 import { hasCookie, getCookie } from "cookies-next";
 // This is a client component
 
-export default function Admin({children }) {
+export default function Admin({ children }) {
   const router = useRouter();
-  
+
   useEffect(() => {
     checkUser();
-  }, [])
+  }, []);
 
-  function checkUser(){
-        const userUUID = getCookie("user_uuid");
-        if (!userUUID){
-          router.replace('/sign-in');
-        }
-        try
-          {
-            setTimeout(async() => {
-              await client.auth.get(`http://localhost:8006/api/get-user-by-id?user_id=${userUUID}`).then((res) => {
-              if(res.data.data.role !== 'ADMIN'){
-                router.replace('/sign-in');
-              }
-            })
-          }, 1);
-          } catch (err) {
-            console.log(err);
-          }
-      }
+  function checkUser() {
+    const userUUID = getCookie("user_uuid");
+    if (!userUUID) {
+      router.replace("/sign-in");
+    }
+    try {
+      setTimeout(async () => {
+        await client.auth
+          .get(`http://localhost:8006/api/get-user-by-id?user_id=${userUUID}`)
+          .then((res) => {
+            if (res.data.data.role !== "ADMIN") {
+              router.replace("/sign-in");
+            }
+          });
+      }, 1);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
-    <Layout><main >{children}</main></Layout>
+    <Layout>
+      <main>{children}</main>
+    </Layout>
   );
 }
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,5 +1,12 @@
 "use client";
-import { Button, Divider, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  TextField,
+  Typography,
+  Chip,
+  Modal,
+} from "@mui/material";
 import Layout from "../../components/client-layout/layout";
 // import QuestionCard from "./question-card";
 import QuestionCard from "../../components/client-layout/card-item/card-question";
@@ -7,20 +14,25 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import client from "../../configs/axios.config";
-
 export default function Profile() {
   const router = useRouter();
   const { id } = router.query;
 
   const [tag, setTag] = useState("question");
-  const [user, setUser] = useState({});
   const [question, setQuestion] = useState([]);
+  const [user, setUser] = useState({});
+
+  const [fullName, setFullName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     console.log(id);
     getUser();
     getQuestion();
-  }, [id]);
+  }, [id, isUpdate]);
 
   async function getUser() {
     try {
@@ -28,12 +40,17 @@ export default function Profile() {
         `http://localhost:8006/api/get-user-by-id?user_id=${id}`
       );
       const data = await res.data.data;
+
       setUser({
         name: data.full_name,
         email: data.email,
         point: data.user_points,
         avatar: data.image_url,
+        status: data.account_status,
       });
+
+      setFullName(data.full_name);
+      setAvatar(data.image_url);
     } catch (err) {
       console.log(err);
     }
@@ -53,14 +70,104 @@ export default function Profile() {
     }
   }
 
+  async function handleUpdateInfo() {
+    try {
+      if (checkImage(avatar) === false) {
+        setError("Avatar URL is not valid, please try again");
+        return;
+      }
+
+      const res = await client.auth.put(
+        `http://localhost:8006/api/user-update-me`,
+        {
+          user_id: id,
+          full_name: fullName,
+          image_url: avatar,
+        }
+      );
+      const data = await res.data.data;
+      console.log(data);
+      setError("");
+      setOpenModal(false);
+      setIsUpdate(!isUpdate);
+    } catch (err) {
+      setError("Full name and/or avatar URL is not valid, please try again");
+      console.log(err);
+    }
+  }
+
+  const modalStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const paperStyle = {
+    backgroundColor: "#fff",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+    padding: "16px",
+    minWidth: "300px",
+  };
+
   return (
     <div className="flex flex-row px-16 py-5">
+      <Modal open={openModal} style={modalStyle}>
+        <div style={paperStyle}>
+          <Typography variant="h6">Update Information</Typography>
+          {/* Error Message */}
+          <Typography
+            sx={{
+              fontSize: "14px",
+              fontWeight: "400",
+              lineHeight: "16px",
+              color: "#FF0000",
+            }}
+          >
+            {error}
+          </Typography>
+
+          <TextField
+            label="Full Name"
+            value={fullName}
+            onChange={() => setFullName(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Avatar URL"
+            value={avatar}
+            onChange={() => setAvatar(event.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <div className="flex flex-row justify-end gap-2 pt-4">
+            <Button variant="filled" color="success" onClick={handleUpdateInfo}>
+              Update
+            </Button>
+            <Button
+              variant="outlined"
+              color="success"
+              onClick={() => setOpenModal(false)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
       <div className="bg-white flex flex-col gap-3 px-2 p-5 rounded-lg">
         <div className="flex flex-row justify-center">
-          <img src={user.avatar} className="rounded-full object-fill w-1/2" />
+          <img
+            src={
+              user.avatar
+                ? user.avatar
+                : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"
+            }
+            className="rounded-full object-fill w-1/2"
+          />
         </div>
         <div className="flex flex-col gap-2">
-          <Button
+          {/* <Button
             variant="outlined"
             sx={{
               borderColor: "#82D200",
@@ -71,7 +178,7 @@ export default function Profile() {
             }}
           >
             Upload avatar
-          </Button>
+          </Button> */}
           <Button
             variant="outlined"
             sx={{
@@ -81,8 +188,9 @@ export default function Profile() {
               fontWeight: 600,
               fontSize: 16,
             }}
+            onClick={() => setOpenModal(true)}
           >
-            Upload information
+            Update information
           </Button>
           <Button
             variant="outlined"
@@ -97,7 +205,23 @@ export default function Profile() {
             Change Password
           </Button>
         </div>
-        <div>
+        <div className="flex flex-col gap-2">
+          {user.status === "pending" ? (
+            <Chip
+              label="Pending"
+              color="primary"
+              variant="outlined"
+              className="py-2"
+            />
+          ) : (
+            <Chip
+              label="Active"
+              color="success"
+              variant="outlined"
+              className="py-2"
+            />
+          )}
+
           <Typography
             sx={{ fontSize: "14px", fontWeight: "400", lineHeight: "16px" }}
           >
@@ -135,7 +259,7 @@ export default function Profile() {
           <Typography
             sx={{ fontSize: "14px", fontWeight: "400", lineHeight: "16px" }}
           >
-            Uuser Point
+            User Point
           </Typography>
           <TextField
             disabled
@@ -172,7 +296,7 @@ export default function Profile() {
             }}
           >
             <span className="pr-1">Question</span>
-            <p>({23})</p>
+            <p>({question.length})</p>
           </Button>
           {/* <Button
             variant="outlined"
